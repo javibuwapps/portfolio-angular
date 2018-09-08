4a0c0fb71f64fc9d63ca7da85013be0a72af3268
javi <dwec.iesvdm@gmail.com>
2018-09-08 14:10:08 +0200

    Fin Seccion 3

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+// app routes
+const routes: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortafolioComponent,
+  },
+  {
+      path: 'about' ,
+      component: AboutComponent
+  },
+  {
+      path: 'item',
+      component: ItemComponent
+  },
+  {
+      path: '**' ,
+      pathMatch: 'full' , redirectTo: 'home'
+  }
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  exports: [
+      RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
